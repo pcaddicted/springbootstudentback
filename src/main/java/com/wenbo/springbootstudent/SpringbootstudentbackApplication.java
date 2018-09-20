e3179a6b4054d323082ee1dbdd80adc06e03d85c
wenbo <wenbo@wenbodeMacBook-Pro.local>
2018-09-20 22:44:55 +0800

    Initial commit

---
+++ src/main/java/com/wenbo/springbootstudent/SpringbootstudentbackApplication.java
@@ -0,0 +1,12 @@
+package com.wenbo.springbootstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstudentbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootstudentbackApplication.class, args);
+    }
+}
